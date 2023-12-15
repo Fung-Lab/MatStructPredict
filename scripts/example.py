f149597b6969fa21f4f4879abb8cb42554293190
vxfung <victorfung88@hotmail.com>
2023-12-15 00:12:13 -0500

    updated example

--- scripts/example.py
+++ scripts/example.py
@@ -1,9 +1,9 @@
-from msp.dataset.dataset import download_dataset, load_dataset, combine_dataset, update_dataset
-from msp.composition.composition import sample_composition_random
-from msp.surrogate.surrogate import Surrogate
-from msp.structure.prediction import Prediction
-from msp.structure.globalopt.basin_hopping import BasinHopping
-from msp.validate.dft_validate import read_dft_config, setup_DFT, Validate
+from msp.dataset import download_dataset, load_dataset, combine_dataset, update_dataset
+from msp.composition import sample_composition_random
+from msp.forcefield import MDL_FF
+from msp.structure.globalopt.basin_hopping import BasinHoppingASE, BasinHopping
+from msp.utils.objectives import UpperConfidenceBound
+from msp.validate import read_dft_config, setup_DFT, Validate
 
 from matdeeplearn.common.ase_utils import MDLCalculator
 
@@ -11,47 +11,60 @@ from matdeeplearn.common.ase_utils import MDLCalculator
 #return dataset class or dict
 my_dataset = download_dataset(repo="MP", save=True)
 #or load dataset from disk:
-#my_dataset = load_dataset(path ="my_path")
+my_dataset = load_dataset(path ="path/to/dataset")
 
 
-#sample composition using a built in random sampler that checks for repeats in the dataset
-#returns a list of compositions, could be length 1 or many
-compositions = sample_composition_random(dataset=my_dataset, n=5)
-compositions=["TiO2"]
-
-#Surrogate class that takes in calculator as an argument, but may have additional functions
-surrogate = Surrogate()
-#train the surrogate (optional)
-#matdeeplearn.train(my_dataset)
-surrogate.train(my_dataset)
-
-#get ase calculator (can be any valid ase calculator)
-calc_str = './configs/config_calculator.yml'
-calculator = MDLCalculator(config=calc_str)
-
-
-basinhop = BasinHopping(calculator=MDLCalculator, hops=5, steps=100, optimizer="FIRE")
-#predict structure returns a list of minima, could be 1 or many
-predictor = Prediction(Surrogate, method = basinhop)
-
-minima_list=[]
-for i in range(0, len(compositions)):
-    putative_minima = predictor.predict(compositions[i], topk=1)
-    minima_list.append(putative_minima[0])
-
-#validate with DFT on-demand
-dft_path = 'path/to/dft_config'
-dft_config=read_dft_config(dft_config)
-method = setup_DFT(dft_config)
-validator = Validate(method=method, local=False)
-dft_results=[]
-for i in range(0, len(minima_list)):
-    dft_results.append(validator(minima_list[i]))
-
-my_dataset_updated = combine_dataset(my_dataset, dft_results)
-
-#update is a finetuning method, not from scratch
-#matdeeplearn.update(my_dataset_updated)
-surrogate.update(my_dataset_updated)
-
-update_dataset(repo="MP", dft_results=dft_results)
+#active learning loop
+for i in range(0, max_iterations):
+    #sample composition using a built in random sampler that checks for repeats in the dataset
+    #returns a list of compositions, could be length 1 or many
+    #compositions are a dictionary of {element:amount}
+    compositions = sample_composition_random(dataset=my_dataset, n=5)
+    #or manually specify the list of dict:
+    compositions=[{'Ti':2, 'O':1}, {'Al':2, 'O':3}]
+    
+    
+    #Initialize a forcefield class, reading in from config (we use MDL_FF but it can be a force field from another library)
+    config = 'path/to/forcefield_config.yml'
+    forcefield = MDL_FF(config=config)
+    #train the forcefield (optional)
+    forcefield.train(my_dataset)
+    
+    
+    #forcefield itself is not an ase calculator, but can be used to return the MDLCalculator class
+    forcefield_calc = forcefield.create_ase_calc()
+    #initialize the predictor class, this is the BasinHopping version which uses an ASE calculator, but we can have another version for batched search
+    predictor = BasinHoppingASE(forcefield_calc, hops=5, steps=100, optimizer="FIRE")
+    #alternatively if we dont use ASE, we can optimize in batch, and optimize over multiple objectives as well
+    #we do this by first initializing our objective function, which is similar to the loss function class in matdeeplearn
+    objective_func = UpperConfidenceBound(c=0.1)
+    predictor = BasinHopping(forcefield, hops=5, steps=100, optimizer="Adam", batch_size=100, objective_func=objective_func)
+    
+    
+    #predict structure returns a list of minima, could be 1 or many
+    minima_list=[]
+    for j in range(0, len(compositions)):
+        putative_minima = predictor.predict(compositions[j], topk=1)
+        minima_list.append(putative_minima[0])
+    
+    
+    #validate with DFT on-demand on the putative minima
+    dft_path = 'path/to/dft_config.yml'
+    dft_config=read_dft_config(dft_config)
+    method = setup_DFT(dft_config)
+    validator = Validate(method=method, local=False)
+    dft_results=[]
+    for j in range(0, len(minima_list)):
+        dft_results.append(validator(minima_list[j]))
+    
+    
+    my_dataset = combine_dataset(my_dataset, dft_results)
+    
+    #retrain the forcefield
+    forcefield.train(my_dataset)
+    #or finetune the forcefield rather than from scratch
+    forcefield.update(dft_results)
+    
+    
+    #update the dataset as well
+    update_dataset(repo="MP", data=dft_results)  
